e733ab877e09aa05c36bb2ee0d6f3060beaa4bb7
Maarti <bryan.martinet@gmail.com>
2021-03-06 16:02:31 -0500

    Support for lists in documentation

--- doc-scraper/index.ts
+++ doc-scraper/index.ts
@@ -79,9 +79,12 @@ function toMarkdown(html: string | null): string {
     .replace(/<\/?code>/gi, "`")          // replace <code></code> tags by `
     .replace(/<pre>/gi, "\n```groovy\n")  // replace <pre> tag by ```groovy
     .replace(/<\/pre>/gi, "\n```\n")      // replace </pre> tag by ```
-    .replace(/<\/?strong>/gi, "**")       // replace <strong></strong> tags by **
+    .replace(/<\/?strong>/gi, "**")       // replace <strong></strong> tags by **    
     .replace(/<h3>/gi, "\n### ")          // replace <h3> tag by \n### 
     .replace(/<\/h3>/gi, "\n")            // replace <h3> tag by \n
+    .replace(/<\/?[u,o]l>/gi, "")         // remove <ul></ul><ol></ol>
+    .replace(/<li>/gi, "\n* ")            // replace <li> tag by \n*
+    .replace(/<\/li>/gi, "\n")            // replace </li> tag by \n
     .replace(/<\/?p>/gi, "\n")            // replace <p></p> tags by \n
     .replace(/<\/?div>/gi, "\n")          // replace <div></div> tags by \n
     .replace(/<br\/?>/gi, "\n\n")         // replace <br> tag by \n\n
